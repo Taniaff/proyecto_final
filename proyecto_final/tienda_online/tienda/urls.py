e53c6ce5a7f4e048fdd15439341a08d6c1c5009b
proyecto <proyecto@example.com>
2016-03-03 08:48:48 +0100

    2

--- proyecto_final/tienda_online/tienda/urls.py
+++ proyecto_final/tienda_online/tienda/urls.py
@@ -3,5 +3,7 @@ from tienda import views
 
 urlpatterns = [
    url(r'^$', views.indice, name='indice'),
-		
+   url(r'^registro$', views.registro, name='registro'),
+   url(r'^login$', views.loginpage, name='login'),
+   url(r'^logout$', views.logoutpage, name='logout'),	
 ]
--- proyecto_final/tienda_online/tienda/views.py
+++ proyecto_final/tienda_online/tienda/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
 from tienda.models import articulos,comentarios,pedido,linea,usuarios
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponseRedirect
+
 # Create your views here.
+
 def indice(request):
    lista_articulos = articulos.objects.all()
    return render(request, 'tienda/index.html', {'lista_articulos': lista_articulos })
+
+def registro(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			new_user = form.save()
+			return HttpResponseRedirect("/")
+	else:
+		form = UserCreationForm()
+	return render(request, "tienda/registro.html", {
+'form': form,})
+
+def loginpage(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(request.POST)
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username,password=password)
+		if user is not None:
+			login(request,user)
+			return HttpResponseRedirect("/")
+	else:
+		form = AuthenticationForm()
+	return render(request,'tienda/login.html', {'form': form,})
+
+def logoutpage(request):
+	logout(request)
+	return HttpResponseRedirect("/")
