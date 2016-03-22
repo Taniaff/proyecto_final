b5bc12360f149d4e0cce884bca956deb25fcc0a9
proyecto <proyecto@example.com>
2016-03-22 10:35:43 +0100

    11

--- proyecto_final/tienda_online/tienda/models.py
+++ proyecto_final/tienda_online/tienda/models.py
@@ -9,7 +9,7 @@ class articulos(models.Model):
 	modelo = models.CharField(max_length=50)
 	color = models.CharField(max_length=20)
 	caracteristicas = models.CharField(max_length=200)
-	precio = models.CharField(max_length=10)
+	precio = models.IntegerField()
 	foto = models.ImageField(upload_to='tienda/static/media',null=True)
 	def __str__(self):
 		return '%s' % (self.modelo)
